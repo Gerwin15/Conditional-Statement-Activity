89f2024470dbf441d1bf2a8a85ce83cc762063b4
Gerwin <160306956+Gerwin15@users.noreply.github.com>
2024-03-16 15:32:56 +0800

    Add files via upload

---
+++ compare/compare.php
@@ -0,0 +1,12 @@
+<?php
+$a = $_GET["1"];
+$b = $_GET["2"];
+    if($a > $b){
+        echo $c = $a." is greater than ".$c = $b;
+    }elseif($a < $b){
+        echo $c = $a." is less than ".$c = $b;
+    }else{
+        echo $c = $a." is equal to ".$c = $b;
+    }
+
+?>
---
+++ compare/input.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="compare.php" method="GET">
+        Enter first number: <input type="text" name="1"><br>
+        Enter second number: <input type="text" name="2"><br>
+        <input type="submit">
+    </form>
+
+    </body>
+</html>
