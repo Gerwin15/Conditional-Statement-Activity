0a6fb654a80240022f744193a5c5d2f454b12e1e
Gerwin <160306956+Gerwin15@users.noreply.github.com>
2024-03-16 15:03:51 +0800

    Age verification

---
+++ age-verfication/age.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <form action="verify.php" method="GET">
+        Enter your age: <input type="text" name="1"><br>
+        <input type="submit">
+    </form>
+
+    </body>
+</html>
---
+++ age-verfication/verify.php
@@ -0,0 +1,9 @@
+<?php 
+$a = $_GET["1"];
+    if($a < 18){
+        echo "You are too young to enter";
+    }
+    else{
+        echo "You are old enough to enter";
+    }
+?>
